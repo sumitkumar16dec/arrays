56e2767ac66430ea95e5cfe7c21dc0487faeeafb
SUMIT KUMAR <77963232+sumitkumar16dec@users.noreply.github.com>
2022-01-18 12:13:54 +0530

    Update selection sort.cpp

--- sorting in arrays/selection sort.cpp
+++ sorting in arrays/selection sort.cpp
@@ -25,3 +25,36 @@ int main(){
 
   return 0;
 }
+
+
+
+"""""""""" OR """"""""""
+
+  
+  
+  #include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n; int arr[1000];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        
+        for(int i=0;i<n-1;i++){
+          int minIndex=i;
+          for(int j=i+1;j<n;j++){
+            if (arr[j]<arr[minIndex]) minIndex=j;
+          }
+          swap(arr[i],arr[minIndex]);
+        }
+    
+        for(int i=0;i<n;i++){
+          cout<<arr[i]<<" ";
+        }
+    }
+
+return 0;
+}
