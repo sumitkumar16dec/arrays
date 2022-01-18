bf00a2bc600fdc7c355dc55bf28f6e608d187292
SUMIT KUMAR <77963232+sumitkumar16dec@users.noreply.github.com>
2022-01-18 18:19:29 +0530

    Update insertion sort.cpp

--- sorting in arrays/insertion sort.cpp
+++ sorting in arrays/insertion sort.cpp
@@ -25,3 +25,38 @@ int main(){
 
   return 0;
 }
+
+
+
+"""""""""" OR """"""""""
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n; int arr[100000];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        
+        for(int i=1;i<n;i++){
+            int temp=arr[i]; int j=i-1;
+            for(;j>=0;j--){
+                if(arr[j]>temp) arr[j+1]=arr[j];
+                else break;
+            }
+            arr[j+1]=temp;
+        }
+        
+        for(int i=0;i<n;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
