d82e11d014f65326de408e570d5201a017a75c2e
SUMIT KUMAR <77963232+sumitkumar16dec@users.noreply.github.com>
2022-01-18 13:56:13 +0530

    Update bubble sort.cpp

--- sorting in arrays/bubble sort.cpp
+++ sorting in arrays/bubble sort.cpp
@@ -29,3 +29,34 @@ int main(){
 
   return 0;
 }
+
+
+
+"""""""""" OR """"""""""
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n; int arr[1000];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        
+        for(int i=1;i<n;i++){
+          for(int j=0;j<n-i;j++){
+            if (arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);;
+          }
+        }
+    
+        for(int i=0;i<n;i++){
+          cout<<arr[i]<<" ";
+        }
+    }
+
+return 0;
+}
